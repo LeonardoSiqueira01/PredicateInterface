bc85e5a30bcb580b8079f47bdef7072717cecc52
Leonardo Siqueira <leonardo.gabriel01@hotmail.com>
2023-02-13 21:37:43 -0300

    Implementando método removeIf utilizando a interface Predicate<T>

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,7 +9,6 @@ import util.ProductPredicate;
 public class Program {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
-		
 		List<Product> list = new ArrayList<>();
 
 		list.add(new Product("Tv", 900.00));
