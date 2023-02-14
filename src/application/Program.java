74a03b7ad04fb0cb3b0b0e9b2cd8878035c1ebb3
Leonardo Siqueira <leonardo.gabriel01@hotmail.com>
2023-02-13 21:54:40 -0300

    Utilizando o predicate através de uma expressão lambda inline(Passando por argumento - sem declarar metodos)

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,7 +3,6 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import java.util.function.Predicate;
 
 import entities.Product;
 
@@ -16,12 +15,10 @@ public class Program {
 		list.add(new Product("mouse", 50.00));
 		list.add(new Product("hd case", 80.90));
 		list.add(new Product("Tablet", 450.00));
-		
-		double min = 100.0;
-		
-		Predicate<Product> pred = p -> p.getPrice() >= min;
 
-		list.removeIf(pred);
+		double min = 100.0;
+
+		list.removeIf(p -> p.getPrice() >= min);
 
 		for (Product p : list) {
 			System.out.println(p);
